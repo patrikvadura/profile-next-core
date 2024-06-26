97503c188eea6927697a198366b8286cc513d6b9
Patrik Vaďura <patrikvadura@icloud.com>
2024-06-26 20:09:04 +0200

    vercel variables api url

--- app/websites/[uniqueKey]/page.tsx
+++ app/websites/[uniqueKey]/page.tsx
@@ -48,251 +48,158 @@ export default async function Home({ params }: { params: { uniqueKey: string } }
     return notFound()
   }
 
-  // Destrukturování dat s výchozími hodnotami
-  const {
-    navigationItems = [],
-    fontName = 'Default Font',
-    fontWeights = '400;700',
-    globalPrimary = '#000000',
-    globalSecondary = '#FFFFFF',
-    globalAccent = '#FF0000',
-    siteName = 'Default Site Name',
-    siteNameClaim = 'Default Site Claim',
-    logoImage = false,
-    logoImageUrl = '',
-    logoImageWidth = 0,
-    logoImageHeight = 0,
-    logoImageSize = 100,
-    cookieShow = false,
-    boxesSocialSites = [],
-    heroVariant = 'defaultVariant',
-    heroImageOpacity = '1',
-    heroAlign = 'center',
-    heroBackground = '#FFFFFF',
-    heroAccentBg = '#000000',
-    heroAccentFg = '#FFFFFF',
-    heroTypo = '#000000',
-    heroTypoLg = '#FFFFFF',
-    heroContentTitle = 'Default Title',
-    heroContentSubtitle = 'Default Subtitle',
-    heroContentLargeTitle = 'Default Large Title',
-    heroContentButtonPrimaryText = 'Default Button Text',
-    heroContentButtonPrimaryLink = '#',
-    heroContentButtonPrimaryCustomLink = '#',
-    heroContentButtonSecondaryText = 'Default Button Text',
-    heroContentButtonSecondaryLink = '#',
-    heroContentButtonSecondaryCustomLink = '#',
-    imageMainUrl = '',
-    imageMainWidth = 0,
-    imageMainHeight = 0,
-    imageBackgroundUrl = '',
-    imageBackgroundWidth = 0,
-    imageBackgroundHeight = 0,
-    showHero = false,
-    showAbout = false,
-    showServices = false,
-    showReference = false,
-    showContact = false,
-    showHeroContentButtonPrimary = false,
-    showHeroContentButtonSecondary = false,
-    showAboutContentButton = false,
-    showContactFormOrMap = false,
-    aboutVariant = 'defaultVariant',
-    aboutLayout = 'defaultLayout',
-    aboutAlign = 'left',
-    aboutOrder = 'asc',
-    aboutBackground = '#FFFFFF',
-    aboutAccentBg = '#000000',
-    aboutAccentFg = '#FFFFFF',
-    aboutTypo = '#000000',
-    aboutContentTitle = 'Default Title',
-    aboutContentDescription = 'Default Description',
-    aboutContentButtonTitle = 'Default Button Text',
-    aboutContentButtonLink = '#',
-    aboutContentButtonCustomLink = '#',
-    boxesAbout = [],
-    serviceVariant = 'defaultVariant',
-    serviceLayout = 'defaultLayout',
-    serviceAlign = 'left',
-    serviceRadius = 'defaultRadius',
-    serviceBackground = '#FFFFFF',
-    serviceAccentBg = '#000000',
-    serviceAccentFg = '#FFFFFF',
-    serviceTypo = '#000000',
-    serviceBoxBackground = '#FFFFFF',
-    serviceBoxTypo = '#000000',
-    serviceBoxIcon = '#000000',
-    servicesContentTitle = 'Default Content Title',
-    boxesService = [],
-    servicesContentBoxSpecial = false,
-    servicesContentBoxSpecialTitle = 'Default Title',
-    servicesContentBoxSpecialLink = '#',
-    servicesContentBoxSpecialCustomLink = '#',
-    referenceVariant = 'defaultVariant',
-    referenceLayout = 'defaultLayout',
-    referenceAlign = 'left',
-    referenceBackground = '#FFFFFF',
-    referenceAccentBg = '#000000',
-    referenceTypo = '#000000',
-    referenceTypoLg = '#FFFFFF',
-    referenceContentTitle = 'Default Title',
-    boxesReference = [],
-    contactVariant = 'defaultVariant',
-    contactLayout = 'defaultLayout',
-    contactAlign = 'left',
-    contactOrder = 'asc',
-    contactBackground = '#FFFFFF',
-    contactAccentBg = '#000000',
-    contactAccentFg = '#FFFFFF',
-    contactTypo = '#000000',
-    contactRecipient = 'default@contact.com',
-    contactMapAddress = 'Default Address',
-    contactContentTitle = 'Default Title',
-    contactContentSubtitle = 'Default Subtitle',
-    contactContentInfoEmail = 'default@contact.com',
-    contactContentInfoPhone = '000-000-0000',
-    contactContentInfoAddress = 'Default Address',
-  } = data
+  const navigationItems = data.navigationItems || []
+  const visibility = {
+    showHero: data.showHero || false,
+    showAbout: data.showAbout || false,
+    showServices: data.showServices || false,
+    showReference: data.showReference || false,
+    showContact: data.showContact || false,
+    showHeroContentButtonPrimary: data.showHeroContentButtonPrimary || false,
+    showHeroContentButtonSecondary: data.showHeroContentButtonSecondary || false,
+    showAboutContentButton: data.showAboutContentButton || false,
+    showContactFormOrMap: data.showContactFormOrMap || false,
+  }
 
   return (
     <>
-      <DynamicFontLoader fontName={fontName} fontWeights={fontWeights} />
+      <DynamicFontLoader fontName={data.fontName} fontWeights={data.fontWeights} />
 
       <ColorUpdaterGlobal
-        primaryGlobalColor={globalPrimary}
-        secondaryGlobalColor={globalSecondary}
-        accentGlobalColor={globalAccent}
+        primaryGlobalColor={data.globalPrimary || ''}
+        secondaryGlobalColor={data.globalSecondary || ''}
+        accentGlobalColor={data.globalAccent || ''}
       />
 
       <div className="fontDefault">
         <Header
-          siteName={siteName}
-          siteNameClaim={siteNameClaim}
-          logoImage={logoImage}
-          logoImageUrl={logoImageUrl}
-          logoImageWidth={logoImageWidth}
-          logoImageHeight={logoImageHeight}
-          logoImageSize={logoImageSize}
-          visibility={{ showHero, showAbout, showServices, showReference, showContact }}
-          navigationItems={navigationItems}
+          siteName={data.siteName || ''}
+          siteNameClaim={data.siteNameClaim || ''}
+          logoImage={data.logoImage || ''}
+          logoImageUrl={data.logoImageUrl || ''}
+          logoImageWidth={data.logoImageWidth || ''}
+          logoImageHeight={data.logoImageHeight || ''}
+          logoImageSize={data.logoImageSize || ''}
+          visibility={visibility || ''}
+          navigationItems={navigationItems || ''}
         />
 
-        {showHero && (
+        {visibility.showHero && (
           <Hero
-            variant={heroVariant}
-            imageOpacity={heroImageOpacity}
-            align={heroAlign}
-            backgroundColor={heroBackground}
-            accentBgColor={heroAccentBg}
-            accentFgColor={heroAccentFg}
-            typoColor={heroTypo}
-            typoLgColor={heroTypoLg}
-            contentTitle={heroContentTitle}
-            contentSubtitle={heroContentSubtitle}
-            contentLargeTitle={heroContentLargeTitle}
-            contentButtonPrimary={showHeroContentButtonPrimary}
-            contentButtonPrimaryText={heroContentButtonPrimaryText}
+            variant={data.heroVariant || ''}
+            imageOpacity={data.heroImageOpacity || ''}
+            align={data.heroAlign || ''}
+            backgroundColor={data.heroBackground || ''}
+            accentBgColor={data.heroAccentBg || ''}
+            accentFgColor={data.heroAccentFg || ''}
+            typoColor={data.heroTypo || ''}
+            typoLgColor={data.heroTypoLg || ''}
+            contentTitle={data.heroContentTitle || ''}
+            contentSubtitle={data.heroContentSubtitle || ''}
+            contentLargeTitle={data.heroContentLargeTitle || ''}
+            contentButtonPrimary={visibility.showHeroContentButtonPrimary || ''}
+            contentButtonPrimaryText={data.heroContentButtonPrimaryText || ''}
             contentButtonPrimaryLink={
-              heroContentButtonPrimaryLink === null
-                ? heroContentButtonPrimaryCustomLink
-                : heroContentButtonPrimaryLink
+              data.heroContentButtonPrimaryLink === null
+                ? data.heroContentButtonPrimaryCustomLink
+                : data.heroContentButtonPrimaryLink
             }
-            contentButtonSecondary={showHeroContentButtonSecondary}
-            contentButtonSecondaryText={heroContentButtonSecondaryText}
+            contentButtonSecondary={visibility.showHeroContentButtonSecondary || ''}
+            contentButtonSecondaryText={data.heroContentButtonSecondaryText || ''}
             contentButtonSecondaryLink={
-              heroContentButtonSecondaryLink === null
-                ? heroContentButtonSecondaryCustomLink
-                : heroContentButtonSecondaryLink
+              data.heroContentButtonSecondaryLink === null
+                ? data.heroContentButtonSecondaryCustomLink
+                : data.heroContentButtonSecondaryLink
             }
-            imageMainUrl={imageMainUrl}
-            imageMainWidth={imageMainWidth}
-            imageMainHeight={imageMainHeight}
-            imageBackgroundUrl={imageBackgroundUrl}
-            imageBackgroundWidth={imageBackgroundWidth}
-            imageBackgroundHeight={imageBackgroundHeight}
+            imageMainUrl={data.imageMainUrl || ''}
+            imageMainWidth={data.imageMainWidth || ''}
+            imageMainHeight={data.imageMainHeight || ''}
+            imageBackgroundUrl={data.imageBackgroundUrl || ''}
+            imageBackgroundWidth={data.imageBackgroundWidth || ''}
+            imageBackgroundHeight={data.imageBackgroundHeight || ''}
           />
         )}
-        {showAbout && (
+        {visibility.showAbout && (
           <About
-            variant={aboutVariant}
-            layout={aboutLayout}
-            align={aboutAlign}
-            order={aboutOrder}
-            backgroundColor={aboutBackground}
-            accentBgColor={aboutAccentBg}
-            accentFgColor={aboutAccentFg}
-            typoColor={aboutTypo}
-            contentTitle={aboutContentTitle}
-            contentDescription={aboutContentDescription}
-            contentButton={showAboutContentButton}
-            contentButtonText={aboutContentButtonTitle}
+            variant={data.aboutVariant || ''}
+            layout={data.aboutLayout || ''}
+            align={data.aboutAlign || ''}
+            order={data.aboutOrder || ''}
+            backgroundColor={data.aboutBackground || ''}
+            accentBgColor={data.aboutAccentBg || ''}
+            accentFgColor={data.aboutAccentFg || ''}
+            typoColor={data.aboutTypo || ''}
+            contentTitle={data.aboutContentTitle || ''}
+            contentDescription={data.aboutContentDescription || ''}
+            contentButton={visibility.showAboutContentButton || ''}
+            contentButtonText={data.aboutContentButtonTitle || ''}
             contentButtonLink={
-              aboutContentButtonLink === null
-                ? aboutContentButtonCustomLink
-                : aboutContentButtonLink
+              data.aboutContentButtonLink === null
+                ? data.aboutContentButtonCustomLink
+                : data.aboutContentButtonLink
             }
-            boxes={boxesAbout}
+            boxes={data.boxesAbout || ''}
           />
         )}
-        {showServices && (
+        {visibility.showServices && (
           <Services
-            variant={serviceVariant}
-            layout={serviceLayout}
-            align={serviceAlign}
-            radius={serviceRadius}
-            backgroundColor={serviceBackground}
-            accentBgColor={serviceAccentBg}
-            accentFgColor={serviceAccentFg}
-            typoColor={serviceTypo}
-            boxBackgroundColor={serviceBoxBackground}
-            boxTypoColor={serviceBoxTypo}
-            boxIconColor={serviceBoxIcon}
-            servicesContentTitle={servicesContentTitle}
-            boxes={boxesService}
-            servicesContentBoxSpecial={servicesContentBoxSpecial}
-            servicesContentBoxSpecialTitle={servicesContentBoxSpecialTitle}
+            variant={data.serviceVariant || ''}
+            layout={data.serviceLayout || ''}
+            align={data.serviceAlign || ''}
+            radius={data.serviceRadius || ''}
+            backgroundColor={data.serviceBackground || ''}
+            accentBgColor={data.serviceAccentBg || ''}
+            accentFgColor={data.serviceAccentFg || ''}
+            typoColor={data.serviceTypo || ''}
+            boxBackgroundColor={data.serviceBoxBackground || ''}
+            boxTypoColor={data.serviceBoxTypo || ''}
+            boxIconColor={data.serviceBoxIcon || ''}
+            servicesContentTitle={data.servicesContentTitle || ''}
+            boxes={data.boxesService || ''}
+            servicesContentBoxSpecial={data.servicesContentBoxSpecial || ''}
+            servicesContentBoxSpecialTitle={data.servicesContentBoxSpecialTitle || ''}
             servicesContentBoxSpecialLink={
-              servicesContentBoxSpecialLink === null
-                ? servicesContentBoxSpecialCustomLink
-                : servicesContentBoxSpecialLink
+              data.servicesContentBoxSpecialLink === null
+                ? data.servicesContentBoxSpecialCustomLink
+                : data.servicesContentBoxSpecialLink
             }
           />
         )}
-        {showReference && (
+        {visibility.showReference && (
           <Reference
-            variant={data.referenceVariant || 'defaultVariant'}
-            layout={data.referenceLayout || 'defaultLayout'}
-            align={data.referenceAlign || 'left'}
-            backgroundColor={data.referenceBackground || '#FFFFFF'}
-            accentBgColor={data.referenceAccentBg || '#000000'}
-            typoColor={data.referenceTypo || '#000000'}
-            typoLgColor={data.referenceTypoLg || '#FFFFFF'}
-            referenceContentTitle={data.referenceContentTitle || 'Default Title'}
-            boxes={data.boxesReference || []}
+            variant={data.referenceVariant || ''}
+            layout={data.referenceLayout || ''}
+            align={data.referenceAlign || ''}
+            backgroundColor={data.referenceBackground || ''}
+            accentBgColor={data.referenceAccentBg || ''}
+            typoColor={data.referenceTypo || ''}
+            typoLgColor={data.referenceTypoLg || ''}
+            referenceContentTitle={data.referenceContentTitle || ''}
+            boxes={data.boxesReference || ''}
           />
         )}
-        {showContact && (
+        {visibility.showContact && (
           <Contact
-            variant={data.contactVariant || 'defaultVariant'}
-            layout={data.contactLayout || 'defaultLayout'}
-            align={data.contactAlign || 'left'}
-            order={data.contactOrder || 'asc'}
-            backgroundColor={data.contactBackground || '#FFFFFF'}
-            accentBgColor={data.contactAccentBg || '#000000'}
-            accentFgColor={data.contactAccentFg || '#FFFFFF'}
-            typoColor={data.contactTypo || '#000000'}
-            contactForm={showContactFormOrMap}
-            contactMap={showContactFormOrMap}
-            contactRecipient={data.contactRecipient || 'default@contact.com'}
-            contactMapAddress={data.contactMapAddress || 'Default Address'}
-            contactContentTitle={data.contactContentTitle || 'Default Title'}
-            contactContentSubtitle={data.contactContentSubtitle || 'Default Subtitle'}
-            contactContentInfoEmail={data.contactContentInfoEmail || 'default@contact.com'}
-            contactContentInfoPhone={data.contactContentInfoPhone || '000-000-0000'}
-            contactContentInfoAddress={data.contactContentInfoAddress || 'Default Address'}
+            variant={data.contactVariant || ''}
+            layout={data.contactLayout || ''}
+            align={data.contactAlign || ''}
+            order={data.contactOrder || ''}
+            backgroundColor={data.contactBackground || ''}
+            accentBgColor={data.contactAccentBg || ''}
+            accentFgColor={data.contactAccentFg || ''}
+            typoColor={data.contactTypo || ''}
+            contactForm={visibility.showContactFormOrMap || ''}
+            contactMap={visibility.showContactFormOrMap || ''}
+            contactRecipient={data.contactRecipient || ''}
+            contactMapAddress={data.contactMapAddress || ''}
+            contactContentTitle={data.contactContentTitle || ''}
+            contactContentSubtitle={data.contactContentSubtitle || ''}
+            contactContentInfoEmail={data.contactContentInfoEmail || ''}
+            contactContentInfoPhone={data.contactContentInfoPhone || ''}
+            contactContentInfoAddress={data.contactContentInfoAddress || ''}
           />
         )}
-        <Footer cookieShow={data.cookieShow || false} boxes={data.boxesSocialSites || []} />
+        <Footer cookieShow={data.cookieShow || false} boxes={data.boxesSocialSites || ''} />
       </div>
     </>
   )
