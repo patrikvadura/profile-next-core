ac717a6dd769687c054393edb32a97e3958290df
Patrik Vaďura <patrikvadura@icloud.com>
2024-07-01 21:02:31 +0200

    domain redirect

--- middleware.js
+++ middleware.js
@@ -1,10 +1,27 @@
 import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-export default withAuth({
-  pages: {
-    signIn: '/auth/signin',
+export default withAuth(
+  async function middleware(request) {
+    const url = request.nextUrl.clone()
+    const hostname = request.headers.get('host')
+
+    console.log('Request URL:', url.href)
+    console.log('Hostname:', hostname)
+
+    if (!hostname) {
+      console.log('Hostname not found')
+      return NextResponse.next()
+    }
+
+    return NextResponse.next()
   },
-})
+  {
+    pages: {
+      signIn: '/auth/signin',
+    },
+  },
+)
 
 export const config = {
   matcher: ['/studio/:path*'],
