0b5f80c44a8c54118a47b91e43b88a50bfe2db30
Patrik Vaďura <patrikvadura@icloud.com>
2024-07-01 20:50:53 +0200

    domain redirect

--- middleware.js
+++ middleware.js
@@ -14,15 +14,20 @@ export default withAuth(
       return NextResponse.next()
     }
 
-    // Extract the full hostname (e.g., subdomain.domain.com)
-    const fullDomain = hostname
-    console.log('Full domain:', fullDomain)
+    // Pokud hostname obsahuje více než dva segmenty, je to subdoména
+    const hostSegments = hostname.split('.')
+    if (hostSegments.length > 2) {
+      const subdomain = hostSegments[0] // extrahování subdomény
+      console.log('Subdomain:', subdomain)
 
-    // Rewrite to the corresponding path based on the full domain
-    url.pathname = `/websites/${fullDomain}${url.pathname}`
-    console.log('Rewritten URL:', url.href)
+      // Přepisování cesty na základě subdomény
+      url.pathname = `/websites/${subdomain}${url.pathname}`
+      console.log('Rewritten URL:', url.href)
+      return NextResponse.rewrite(url)
+    }
 
-    return NextResponse.rewrite(url)
+    // Pokud hostname neobsahuje subdoménu, pokračujte bez změny
+    return NextResponse.next()
   },
   {
     pages: {
