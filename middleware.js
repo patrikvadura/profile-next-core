0cca382002e1f4fcad0288fc79e2f9b938257c79
Patrik Vaďura <patrikvadura@icloud.com>
2024-07-01 21:37:15 +0200

    domain redirect

--- middleware.js
+++ middleware.js
@@ -1,33 +1,42 @@
 import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 
-export default withAuth(
-  async function middleware(request) {
-    const url = request.nextUrl.clone()
-    const hostname = request.headers.get('host')
+// Middleware bez autorizace
+async function middlewareWithoutAuth(request) {
+  const url = request.nextUrl.clone()
+  const hostname = request.headers.get('host')
 
-    console.log('Request URL:', url.href)
-    console.log('Hostname:', hostname)
+  console.log('Request URL:', url.href)
+  console.log('Hostname:', hostname)
 
-    if (!hostname) {
-      console.log('Hostname not found')
-      return NextResponse.next()
-    }
-
-    // Remove the port number from the hostname if it exists
-    const hostnameWithoutPort = hostname.split(':')[0]
-
-    // Check if the request is from localhost or not
-    const isLocalhost = hostnameWithoutPort === 'localhost' || hostnameWithoutPort === '127.0.0.1'
-
-    // If not localhost, rewrite the URL to point to the corresponding directory
-    if (!isLocalhost) {
-      url.pathname = `/websites/${hostnameWithoutPort}${url.pathname}`
-      console.log('Rewritten URL:', url.href)
-      return NextResponse.rewrite(url)
-    }
-
+  if (!hostname) {
+    console.log('Hostname not found')
     return NextResponse.next()
+  }
+
+  // Remove the port number from the hostname if it exists
+  const hostnameWithoutPort = hostname.split(':')[0]
+
+  // Check if the request is from localhost or not
+  const isLocalhost = hostnameWithoutPort === 'localhost' || hostnameWithoutPort === '127.0.0.1'
+
+  // Get default domain from environment variables
+  const defaultDomain = process.env.NEXT_PUBLIC_DEFAULT_DOMAIN
+
+  // If not localhost and not the default domain, rewrite the URL to point to the corresponding directory
+  if (!isLocalhost && hostnameWithoutPort !== defaultDomain) {
+    url.pathname = `/websites/${hostnameWithoutPort}${url.pathname}`
+    console.log('Rewritten URL:', url.href)
+    return NextResponse.rewrite(url)
+  }
+
+  return NextResponse.next()
+}
+
+// Middleware s autorizací pouze pro /studio
+const authMiddleware = withAuth(
+  async function middleware(request) {
+    return middlewareWithoutAuth(request)
   },
   {
     pages: {
@@ -36,10 +45,15 @@ export default withAuth(
   },
 )
 
+// Export middleware
+export default function middleware(request) {
+  // Pokud URL obsahuje "/studio", použijte authMiddleware, jinak middlewareWithoutAuth
+  if (request.nextUrl.pathname.startsWith('/studio')) {
+    return authMiddleware(request)
+  }
+  return middlewareWithoutAuth(request)
+}
+
 export const config = {
-  matcher: [
-    // Add paths that need authentication and subdomain routing
-    '/studio/:path*',
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/studio/:path*', '/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
